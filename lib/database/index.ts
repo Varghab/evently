67dc1eb4b887de1c2f6bfd09b4629bce6d610287
Varghab <shibvarghab@gmail.com>
2024-02-02 22:41:39 +0530

    added vercel json file

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,6 +4,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || {conn:null, promise:null};
 export const connectToDatabase = async () => {
+    console.log("Database connection function!");
+    
     if(cached.conn) return cached.conn;
 
     if(!MONGODB_URI) throw new Error("MONGODB_URI is missing");
