6d6805d7aedb4274808306d2428527b3b45bf4d5
Varghab <shibvarghab@gmail.com>
2024-02-03 00:04:04 +0530

    f

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,36 +4,38 @@ import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
-
+ 
 export async function POST(req: Request) {
+ 
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-  
+ 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
-
+ 
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-
+ 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
-
+ 
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
-
+ 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
+ 
   let evt: WebhookEvent
-
+ 
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
@@ -47,12 +49,11 @@ export async function POST(req: Request) {
       status: 400
     })
   }
-
+ 
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log(evt.data);
-  
+ 
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
@@ -98,8 +99,9 @@ export async function POST(req: Request) {
 
     const deletedUser = await deleteUser(id!)
 
-    return Response.json({ message: "Received" });
+    return NextResponse.json({ message: 'OK', user: deletedUser })
   }
  
-  return Response.json({ message: "Received" });
+  return new Response('', { status: 200 })
 }
+ 
