ac70d9a139fc27876d7cc08e8f889c69a5ac83b7
Varghab <shibvarghab@gmail.com>
2024-02-02 23:21:12 +0530

    changed

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -9,7 +9,6 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
   console.log("Request recieved!");
-  console.log(WEBHOOK_SECRET);
   
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
